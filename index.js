fa40e3613e2ce31757ddc6a900c0037425422e34
shheeeeshka <102031292+shheeeeshka@users.noreply.github.com>
2024-11-14 11:10:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ import mailService from "./mailService.js";
 config();
 
 async function main() {
+    const executablePath = process.env.OS === "macos" ? "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" : "";
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: false,
         timeout: 0,
         protocolTimeout: 0,
         userDataDir: "./tmp",
-        executablePath: `/Applications/Google Chrome.app/Contents/MacOS/Google Chrome`,
+        // executablePath,
     });
     const page = await browser.newPage();
 
